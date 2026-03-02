9ea7fc9ade5216e613bf46c8fef19152d8a23dd4
duanhap <congduan2554@gmail.com>
2026-03-02 23:20:39 +0700

    update cònig

--- app/config.py
+++ app/config.py
@@ -1,12 +1,17 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SAKURA_SECRET_KEY", "dev-secret-sakura")
-    DB_USER = os.getenv("SAKURA_DB_USER", "root")
-    DB_PWD = os.getenv("SAKURA_DB_PASSWORD", "duanhap2554")
-    DB_HOST = os.getenv("SAKURA_DB_HOST", "localhost")
-    DB_PORT = os.getenv("SAKURA_DB_PORT", "3306")
-    DB_NAME = os.getenv("SAKURA_DB_NAME", "sakura")
+    SECRET_KEY = os.getenv("SAKURA_SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DB_USER = os.getenv("SAKURA_DB_USER")
+    DB_PWD = os.getenv("SAKURA_DB_PASSWORD")
+    DB_HOST = os.getenv("SAKURA_DB_HOST")
+    DB_PORT = os.getenv("SAKURA_DB_PORT", "3306")
+    DB_NAME = os.getenv("SAKURA_DB_NAME")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PWD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
